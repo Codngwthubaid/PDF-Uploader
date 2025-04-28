0fbf1761fddcf65d120ab727c8c3bbab26f817d4
Codngwthubaid <codngwthubaid@gmail.com>
2025-04-28 10:49:58 +0530

    openai api key arises error

--- server/worker.js
+++ server/worker.js
@@ -2,43 +2,43 @@ import { Worker } from 'bullmq';
 import { OpenAIEmbeddings } from "@langchain/openai";
 import { QdrantVectorStore } from "@langchain/qdrant"
 import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf";
-import dotenv from 'dotenv';
+import dotenv from "dotenv"
 
 dotenv.config()
 
 const myWorker = new Worker('file-upload-queue', async (job) => {
+  try {
     console.log(job.data)
     const data = JSON.parse(job.data)
 
-
     const loader = new PDFLoader(data.filePath);
     const docs = await loader.load();
-    console.log("Docs :", docs)
-
+    console.log("Docs:", docs)
 
     const embeddings = new OpenAIEmbeddings({
-        apiKey: process.env.OPENAI_API_KEY,
-        model: "text-embedding-3-large"
+      apiKey: process.env.OPENAI_API_KEY,
+      model: "text-embedding-3-large"
     });
-    console.log("OpenAI API Key :", process.env.OPENAI_API_KEY)
-    console.log("Qdrant URL :", process.env.QDRANT_URL)
+    console.log("OpenAI API Key:", process.env.OPENAI_API_KEY)
+    console.log("Qdrant URL:", process.env.QDRANT_URL)
 
     const vectorStore = await QdrantVectorStore.fromExistingCollection(embeddings, {
-        url: process.env.QDRANT_URL,
-        collectionName: "langchainjs-testing",
+      url: process.env.QDRANT_URL,
+      collectionName: "langchainjs-testing",
     });
+    console.log("Vector Store:", vectorStore)
 
     await vectorStore.addDocuments(docs);
     console.log("File uploaded and indexed successfully.")
-
+  } catch (error) {
+    console.error("Error while processing the job:", error)
+  }
 }, {
-    concurrency: 100,
-    connection: {
-        host: 'localhost',
-        port: 6379,
-    },
-})
-
-
+  concurrency: 100,
+  connection: {
+    host: 'localhost',
+    port: 6379,
+  },
+});
 
 
