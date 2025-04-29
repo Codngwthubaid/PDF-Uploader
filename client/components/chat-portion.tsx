6f1088158d60cf55f3729e96f73c680c9157c44f
Codngwthubaid <codngwthubaid@gmail.com>
2025-04-29 11:28:15 +0530

    working on UI of chat section

--- client/components/chat-portion.tsx
+++ client/components/chat-portion.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { Button } from "./ui/button";
 import { Input } from "./ui/input";
 import axios from "axios";
+import { UserButton } from "@clerk/nextjs";
 
 
 interface Doc {
@@ -23,24 +24,29 @@ interface IMessage {
 export default function ChatPortion() {
 
     const [message, setMessage] = useState<string>("");
-    console.log("Sending message:", message);
     const [messages, setMessages] = useState<IMessage[]>([]);
-    console.log("All Messages:", messages);
 
     const handleSendChatMessage = async () => {
         setMessages((prev) => [...prev, { role: "user", content: message }]);
         const response = await axios.get(`http://localhost:5000/chat?message=${message}`)
         const data = await response.data;
-        setMessages((prev) => [...prev, { role: "assistant", content: data?.message, document: data?.docs }]);
+        setMessages((prev) => [...prev, { role: "assistant", content: data?.message, document: data?.doc }]);
         console.log("Response:", data);
+        console.log("Source of the result :", data?.doc?.metdata?.source);
+        console.log("Page Number of the result :", data?.doc?.metdata?.loc?.pageNumber);
     }
     return (
         <div className="p-4">
             {messages.map((message, index) => {
                 return (
-                    <div key={index} className={`flex ${message.role === "assistant" ? "flex-row-reverse" : ""}`}>
-                        <div className="w-[60%] border border-gray-700 p-4">
-                            {message.content}
+                    <div className="flex gap-x-4" key={index}>
+                        <div className={`flex ${message.role === "assistant" ? "flex-row-reverse" : ""}`}>
+                            {message.role === "assistant" ? `PDF` : <UserButton />}
+                        </div>
+                        <div className={`flex ${message.role === "assistant" ? "flex-row-reverse" : ""}`}>
+                            <div className="w-[60%] border border-gray-700 p-4 rounded-md my-2">
+                                {message.content}
+                            </div>
                         </div>
                     </div>
                 );
